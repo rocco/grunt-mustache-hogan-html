69f12a9f708169886c4f0bd9e5e3d05d1e99b2c4
Rocco Georgi <rocco@pavingways.com>
2015-02-06 12:46:49 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# grunt-mustache-hogan-html
+
+> Compile mustache or hbs templates using Hogan.js
+
+## Getting Started
+This plugin requires Grunt `~0.4.2`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-mustache-hogan-html --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-mustache-hogan-html');
+```
+
+## The "mustache_hogan_html" task
+
+### Overview
+Generate static HTML files from mustache|hbs templates using Hogan.js.
+Make use of layouts and nested partials.
+
+```js
+grunt.initConfig({
+  mustache_hogan_html: {
+    development: {
+      options: {
+        src: 'src',
+        dist: 'dist',
+        type: 'mustache' // mustache or hbs
+      },
+      globals: {
+        analytics_id: 'UA-123456-1'
+      }
+    }
+  }
+});
+```
+
+Now the subtask `mustache_hogan_html:development` is available. Add further targets (such as `build` as desired).
+
+### Options object
+
+#### options.src
+Type: `String`
+Default value: `src`
+
+The source directory of your templates, the directory should be structured like:
+
+```txt
+src/
+ ├── layout.mustache
+ ├── pages
+ │   └── index.json
+ │   └── index.mustache
+ │   └── 404.json
+ │   └── 404.mustache
+ └── partials
+     └── header.mustache
+     └── nav.mustache
+     └── footer.mustache
+```
+
+* `layout.mustache` must exist 
+* `pages` folder contains the page templates that are transformed into HTML files
+* `.json` files in `pages` folder contain page specify data, can be also rendered in layout page
+* `partials` contains the partial templates that might be used in the page templates
+
+#### options.dist
+Type: `String`
+Default value: `dist`
+
+Destination directory of rendered HTML files.
+
+#### options.type
+Type: `String`
+Default value: `mustache`
+
+The type of template, can be `mustache` or `hbs`.
+
+#### globals
+Type: `Object`
+Default value: {}
+
+Contains global data. Same keys will be overwritten by page-specific values from `.json` files.
+
+## Test
+
+```bash
+grunt test
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style.
+Add unit tests for any new or changed functionality.
+Lint and test your code
+
+## Release History
+_(Nothing yet)_
+
+## Props
+This project is based on [grunt-mustache-html by zhongyu](https://github.com/haio/grunt-mustache-html)
---
+++ tasks/mustache_hogan_html.js
@@ -0,0 +1,216 @@
+/*
+ * grunt-mustache-hogan-html
+ * https://github.com/rocco/grunt-mustache-hogan-html
+ * 
+ * Copyright (c) 2015 rocco
+ * 
+ * based on grunt-mustache-html
+ * https://github.com/haio/grunt-mustache-html
+ *
+ * Copyright (c) 2013 zhongyu
+ 
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('mustache_hogan_html', 'Compile mustache|hbs templates to HTML unsing Hogan.js', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      src: 'src',
+      dist: 'dist',
+      type: 'mustache'
+    });
+
+    var globals = this.data.globals || {};
+
+    var hogan = require('hogan.js'),
+        jstSuffix = '.' + options.type,
+        matcher = new RegExp('\\' + jstSuffix + '$');
+
+   // hogan.compile each partial, return object of compiled partials
+    var compilePartials = function (partialPath) {
+
+        console.log('- using partial path: %s', partialPath);
+
+        var allPartials = {};
+
+        // for all files in dir
+        grunt.file.recurse(partialPath, function (abspath, rootdir, subdir, filename) {
+
+            // file extension does not match
+            if (!filename.match(matcher)) {
+                console.log('-- ignoring file: %s', filename);
+                return;
+            }
+
+            var partialName = filename.replace(matcher, ''),
+                partialSrc = grunt.file.read(abspath);
+
+            console.log('-- compiling partial: %s', filename);
+            allPartials[partialName] = hogan.compile(partialSrc); // , { sectionTags: [{o:'_i', c:'i'}] }
+
+        });
+
+        return allPartials;
+    };
+
+    // hogan render each page, return rendered pages
+    var compilePages = function (pagesPath, allPartials) {
+
+        console.log('- using pages path: %s', pagesPath);
+
+        var allPages = {};
+
+        // for all files in dir
+        grunt.file.recurse(pagesPath, function (abspath, rootdir, subdir, filename) {
+
+            // file extension does not match
+            if (!filename.match(matcher)) {
+                console.log('-- ignoring file: %s', filename);
+                return;
+            }
+
+            var pageName = filename.replace(matcher, ''),
+                pageSrc = grunt.file.read(abspath),
+                pageData = {},
+                dataPath = abspath.replace(matcher, '.json'),
+                locals = merge({}, globals);
+
+console.log('-- compiling page: %s', filename);
+
+            var compiledPage = hogan.compile(pageSrc); // , { sectionTags: [{o:'_i', c:'i'}] }
+
+            // read page data from {pageName}.json
+
+console.log('--- looking for page data in: %s', dataPath);
+
+            if (grunt.file.exists(dataPath)) {
+                pageData = JSON.parse(grunt.file.read(dataPath), function (key, value) {
+                    if (value && (typeof value === 'string') && value.indexOf('function') === 0) {
+                        try {
+                            return new Function('return ' + value)();
+                        } catch (ex) {
+                            //faulty function, just return it as a raw value
+                        }
+                    }
+                    return value;
+                });
+                merge(locals, pageData);
+                pageData[pageName] = locals;
+            }
+
+            allPages[pageName] = compiledPage.render(locals, allPartials);
+
+        });
+
+//console.log(allPages);
+
+        return allPages;
+
+    };
+/*
+    var renderPages = function (path, partials) {
+console.log('rendering path: ', path);
+        var pages = {}; 
+        grunt.file.recurse(path, function (abspath, rootdir, subdir, filename) {
+
+            if (!filename.match(matcher)) {
+                return;
+            }
+
+            var name = filename.replace(matcher, ''),
+                dataPath = abspath.replace(matcher, '.json'),
+                locals = merge({}, globals),
+                data   = {};
+
+console.log('compiling %s', abspath);
+
+            var templateSrc = grunt.file.read(abspath),
+                template = hogan.compile(templateSrc, { sectionTags: [{o:'_i', c:'i'}] });
+
+            if (grunt.file.exists(dataPath)) {
+                data = JSON.parse(grunt.file.read(dataPath), function (key, value) {
+                    if (value && (typeof value === 'string') && value.indexOf('function') === 0) {
+                        try {
+                            return new Function('return ' + value)();
+                        } catch (ex) {
+                            //faulty function, just return it as a raw value
+                        }
+                    }
+                    return value;
+                });
+                merge(locals, data);
+                pageData[name] = locals;
+            }
+
+            pages[name] = template.render(locals, partials);
+        });
+        if (!partials) {
+console.log('recursing: ', path);
+            return renderPages(path, pages);
+        } else {
+            return pages;
+        }
+    };
+*/
+
+    var each = function (obj, iter) {
+        var keys = Object.keys(obj);
+        for (var i=0,l=keys.length; i<l; i++) {
+            iter.call(null, obj[keys[i]], keys[i]);
+        }
+    };
+
+    var merge = function (init, extended) {
+      each(extended, function(v, k) {
+        init[k] = v;
+      });
+
+      return init;
+    };
+
+
+
+
+
+
+
+
+
+
+
+    // get paths
+    var layoutPath  = options.src + '/layout' + jstSuffix,
+        pagePath    = options.src + '/pages',
+        partialPath = options.src + '/partials';
+
+    // render partials and pages with partial data
+    var pageData = {}, // filled in compilePages
+        partials = compilePartials(partialPath),
+        pages    = compilePages(pagePath, partials);
+
+    // get layout and compile it
+    var layoutSrc  = grunt.file.read(layoutPath),
+        layoutComp = hogan.compile(layoutSrc); //, { sectionTags: [{o:'_i', c:'i'}] });
+
+    // for each page (which are only partials after all) render an HTML file
+    each(pages, function (page, name) {
+        // add page as a partial called {{>content}}
+        partials.content = page;
+        // render actual page using layout and all partials (including pages now)
+        var htmlContent = layoutComp.render(pageData[name] || {}, partials);
+        // write HTML file
+        grunt.file.write(options.dist  + '/' + name + '.html', htmlContent);
+        console.log('wrote HTML file: %s', options.dist  + '/' + name + '.html');
+    });
+
+ 
+
+  });
+};
